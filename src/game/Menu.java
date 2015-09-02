33f4e021f9459ec8eb213c44abbe1d9d7b6d5181
Terence Sun <tsun1215@gmail.com>
2015-09-02 12:58:22 -0400

    Changed menu option 2 to start a game with user-defined max value

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -1,41 +1,50 @@
 package game;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
 
-  public static void main(String[] args){
-    Menu m = new Menu();
-    m.displayMenu();
-    m.promptUserInput();
-  }
+	public static void main(String[] args) {
+		Menu m = new Menu();
+		m.displayMenu();
+		m.promptUserInput();
+	}
 
-  public void displayMenu(){
-    System.out.println("Welcome to the Nubmer Guessing Game!");
-    System.out.println("Type 1 to start a new game");
-    System.out.println("Type 2 to quit");
-  }
+	public void displayMenu() {
+		System.out.println("Welcome to the Nubmer Guessing Game!");
+		System.out.println("Type 1 to start a new game");
+		System.out.println("Type 2 to start game with user selected max value");
+	}
 
-  public void promptUserInput(){
-    System.out.print("Please enter your selection: ");
-    Scanner scan = new Scanner(System.in);
-    int choice = 0;
-    try{
-      choice = scan.nextInt();
-      if(choice == 1){
-        GuessingGameLogic gg = new GuessingGameLogic(scan);
-        gg.guessingGame();
-      }else if(choice == 2){
-        System.exit(0);
-      } else{
-        printErrorMessage();
-      }
-    }catch (IllegalStateException e){
-      printErrorMessage();
-    }
-  }
+	public void promptUserInput() {
+		System.out.print("Please enter your selection: ");
+		Scanner scan = new Scanner(System.in);
+		int choice = 0;
+		try {
+			choice = scan.nextInt();
+			if (choice == 1) {
+				GuessingGameLogic gg = new GuessingGameLogic(scan);
+				gg.guessingGame();
+			} else if (choice == 2) {
+				System.out.println("Please enter a max value: ");
+				try {
+					int maxValue = scan.nextInt();
+					GuessingGameLogic gg = new GuessingGameLogic(scan, maxValue);
+					gg.guessingGame();
+				} catch (InputMismatchException e) {
+					printErrorMessage();
+					System.exit(0);
+				}
+			} else {
+				printErrorMessage();
+			}
+		} catch (IllegalStateException e) {
+			printErrorMessage();
+		}
+	}
 
-  public void printErrorMessage(){
-    System.err.println("Invalid input. Please start again.");
-  }
+	public void printErrorMessage() {
+		System.err.println("Invalid input. Please start again.");
+	}
 }
