08d8382e7979996fe5996f8ed7e2457f316d2dd8
Terence Sun <tsun1215@gmail.com>
2015-09-02 12:50:23 -0400

    Changed option 2 in menu to print author info, then exit

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -13,7 +13,7 @@ public class Menu {
   public void displayMenu(){
     System.out.println("Welcome to the Nubmer Guessing Game!");
     System.out.println("Type 1 to start a new game");
-    System.out.println("Type 2 to quit");
+    System.out.println("Type 2 to display author info");
   }
 
   public void promptUserInput(){
@@ -26,6 +26,7 @@ public class Menu {
         GuessingGameLogic gg = new GuessingGameLogic(scan);
         gg.guessingGame();
       }else if(choice == 2){
+    	System.out.println("The author info");
         System.exit(0);
       } else{
         printErrorMessage();
